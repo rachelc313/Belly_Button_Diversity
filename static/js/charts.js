f65c8f218926cacbf1bdf9b5c0a73819d136177f
rachelc313 <111570965+rachelc313@users.noreply.github.com>
2022-11-28 19:31:14 -0600

    Bubble chart completed

--- static/js/charts.js
+++ static/js/charts.js
@@ -117,7 +117,40 @@ function buildMetadata(sample) {
        
       };
       // 10. Use Plotly to plot the data with the layout. 
-      Plotly.newPlot("bar", barData, barLayout)
-    });
-  }
+      Plotly.newPlot("bar", barData, barLayout);
+        // 1. Create the trace for the bubble chart.
+        var trace2 = {
+          x: otuIDs,
+          y: sampleValues,
+          mode: "markers",
+          text: otuLabels,
+          marker: {
+            size: sampleValues,
+            color: otuIDs,
+        },
+          type: "scatter"
+        };
+            
+        var bubbleData = [trace2];
+        // 2. Create the layout for the bubble chart.
+        var bubbleLayout = {
+          title: "Bacteria Cultures Per Sample",
+          xaxis: {title: "OTU ID"},
+          hovermode: otuLabels
+          
+        };
+        
+        // 3. Use Plotly to plot the data with the layout.
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout) 
+        });
+      
+    };
+    
+//     // Bar and Bubble charts
+// // Create the buildCharts function.
+// function buildCharts(sample) {
+//   // Use d3.json to load and retrieve the samples.json file 
+//   d3.json("samples.json").then((data) => {
+    
   
+
